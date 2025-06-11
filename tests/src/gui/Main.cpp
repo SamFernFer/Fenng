579ca0ca3e1d4e9d57ec6fd8b6da50c842b42943
SamFernFer <>
2025-06-11 00:24:55 -0300

    Create the setup for the Hello World program. Haven't tested yet.

---
+++ tests/src/gui/Main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "GUI not implemented yet." << std::endl;
+    return 0;
+}
---
+++ tests/src/window/Main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Window not implemented yet." << std::endl;
+    return 0;
+}
