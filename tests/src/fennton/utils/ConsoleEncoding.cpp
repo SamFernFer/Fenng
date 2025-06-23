c44676dee432a890273980d34e5cf0feaf9166bb
SamFernFer <>
2025-06-23 20:13:46 -0300

    Restructuring the project so that both the tests and main executable are built conditionally, started from the same CMake file.

--- tests/src/fennton/utils/ConsoleEncoding.cpp
+++ tests/src/fennton/utils/ConsoleEncoding.cpp
@@ -1,18 +1,26 @@
 #include <fennton/utils/Console.hpp>
 
 int main() {
-    // TODO:
     // - Input: ASCII and non-ASCII input to the std::cout;
     // - Expected output (manually checked): ASCII is perfect, but non-ASCII text might be broken;
+    std::cout << "This is ASCII text.";
+    std::cout << "Ábaco, Cristóvão, Horário, Müller, Niño, Ýúúý";
+
     Fennton::Console::init();
-    // TODO:
+
     // - Input: ASCII and non-ASCII input to the std::cout;
     // - Expected output (manually checked): all input is rendered perfectly, except characters 
     // not present in the console's font;
+    std::cout << "This is ASCII text.";
+    std::cout << "Ábaco, Cristóvão, Horário, Müller, Niño, Ýúúý";
+
     Fennton::Console::term();
-    // TODO:
+
     // - Input: ASCII and non-ASCII input to the std::cout;
     // - Expected output (manually checked): ASCII is perfect, but non-ASCII text might be broken, 
     // exactly how it was before the call to Fennton::Console::init;
+    std::cout << "This is ASCII text.";
+    std::cout << "Ábaco, Cristóvão, Horário, Müller, Niño, Ýúúý";
+
     return 0;
 }
