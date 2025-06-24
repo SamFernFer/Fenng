dad4827652312eeb49d4885c8062de1b3fd695e6
SamFernFer <>
2025-06-23 21:54:18 -0300

    Console Init Test: added Japanese characters and constexpr variables to avoid repeating the strings.

--- tests/src/fennton/utils/ConsoleInit.cpp
+++ tests/src/fennton/utils/ConsoleInit.cpp
@@ -1,26 +1,30 @@
 #include <fennton/utils/Console.hpp>
 
 int main() {
+    constexpr char const
+        *line1 = "This is ASCII text.\n",
+        *line2 = "Ábaco, Cristóvão, Horário, Müller, Niño, Ýúúý, どうもありがとう\n"
+    ;
     // - Input: ASCII and non-ASCII input to the std::cout;
     // - Expected output (manually checked): ASCII is perfect, but non-ASCII text might be broken;
-    std::cout << "This is ASCII text.\n";
-    std::cout << "Ábaco, Cristóvão, Horário, Müller, Niño, Ýúúý\n";
+    std::cout << line1;
+    std::cout << line2;
 
     Fennton::Console::init();
 
     // - Input: ASCII and non-ASCII input to the std::cout;
     // - Expected output (manually checked): all input is rendered perfectly, except characters 
     // not present in the console's font;
-    std::cout << "This is ASCII text.\n";
-    std::cout << "Ábaco, Cristóvão, Horário, Müller, Niño, Ýúúý\n";
+    std::cout << line1;
+    std::cout << line2;
 
     Fennton::Console::term();
 
     // - Input: ASCII and non-ASCII input to the std::cout;
     // - Expected output (manually checked): ASCII is perfect, but non-ASCII text might be broken, 
     // exactly how it was before the call to Fennton::Console::init;
-    std::cout << "This is ASCII text.\n";
-    std::cout << "Ábaco, Cristóvão, Horário, Müller, Niño, Ýúúý\n";
+    std::cout << line1;
+    std::cout << line2;
 
     // Pauses the console.
     std::cin.get();
