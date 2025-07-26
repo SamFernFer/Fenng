7e1e338b8dcb6cb2ff8a1195e82f825f4eb26157
SamFernFer <>
2025-07-25 23:09:32 -0300

    Found a nicer shade of green.

--- tests/src/fennton/gl/Manipulation.cpp
+++ tests/src/fennton/gl/Manipulation.cpp
@@ -35,7 +35,7 @@ int main() {
             Window::pollEvents();
 
             if (!_win->IsIconified()) {
-                glClearColor(0.1f, 0.4f, 0.05f, 1.0f);
+                glClearColor(0.1f, 0.3f, 0.15f, 1.0f);
                 glClear(GL_COLOR_BUFFER_BIT);
             }
             _win->SwapBuffers();
