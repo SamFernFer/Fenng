331592a5011c340ecb00c232a55fac6ac75bab36
SamFernFer <>
2025-08-25 00:52:57 -0300

    Working on the structure of the Token class and its internal variations.

--- include/fennton/skript/Parser.hpp
+++ include/fennton/skript/Parser.hpp
@@ -1,4 +1,5 @@
 #include <deque>
+#include <vector>
 #include <string>
 #include <variant>
 #include <cstdint>
@@ -6,10 +7,16 @@
 namespace Fennton::Skript {
     namespace Tokeniser {
         class Name {
+        private:
+            std::string spelling;
+        public:
+            bool operator==(Name const& other) const;
+            bool operator!=(Name const& other) const;
         };
         class Number {
+        private:
             // Contiguous string containing the number's parts and suffixes.
-            std::string str;
+            std::string storage;
             // Vector instead of list because it allows random indexing and it is going 
             // to handle the memory allocations and deallocations.
             std::vector<std::string_view> parts;
@@ -18,23 +25,46 @@ namespace Fennton::Skript {
             // The number's base.
             std::int32_t base;
         public:
-            std::int32_t GetBase() const;
+            Number();
+            bool operator==(Number const& other) const;
+            bool operator!=(Number const& other) const;
             std::vector<std::string_view> const& GetParts() const;
             std::vector<std::string_view> const& GetSuffixes() const;
+            std::int32_t GetBase() const;
         };
         class String {
+        public:
+            bool operator==(String const& other) const;
+            bool operator!=(String const& other) const;
         };
         class Punct {
+        public:
+            bool operator==(Punct const& other) const;
+            bool operator!=(Punct const& other) const;
         };
         class Token {
-            std::variant<Name, Number, String, Punct> var;
         public:
+            using VariantType = std::variant<Name, Number, String, Punct>;
+        private:
+            VariantType var;
+            std::uint8_t flags;
+        public:
+            constexpr std::int32_t spaceAfterBit = 0;
             Token();
+            // Compares two tokens for equality.
             bool operator==(Token const& other) const {
                 return var == other.var;
             }
-            // Returns the token's spelling.
-            std::string const& GetSpelling();
+            // Compares two tokens for inequality.
+            bool operator!=(Token const& other) const {
+                return !(*this == other);
+            }
+            // Returns the token's spelling, not exactly equal to how it was spelled in the 
+            // source, but which generates the same token if retokenised.
+            std::string const& GetSpelling() const;
+            // Returns true if there is a space between this token and the next and false if 
+            // there is either no space or no token.
+            bool HasSpaceAfter() const;
         };
     };
     std::deque<Tokeniser::Token> tokenise(std::string_view str);
--- src/fennton/skript/Parser.cpp
+++ src/fennton/skript/Parser.cpp
@@ -1,13 +1,59 @@
 #include <fennton/skript/Parser.hpp>
+#include <type_traits>
 
 namespace Fennton::Skript {
     namespace Tokeniser {
+        bool Name::operator==(Name const& other) const {
+            return spelling == other.spelling;
+        }
+        bool Name::operator!=(Name const& other) const {
+            return !(*this == other);
+        }
+
+        bool Number::operator==(Number const& other) const {
+            return
+                (parts == other.parts)
+                && (suffixes == other.suffixes)
+            ;
+        }
+        bool Number::operator!=(Number const& other) const {
+            return !(*this == other);
+        }
+        std::vector<std::string_view> const& Number::GetParts() const {
+            return parts;
+        }
+        std::vector<std::string_view> const& Number::GetSuffixes() const {
+            return suffixes;
+        }
         std::int32_t Number::GetBase() const {
             return base;
         }
-        std::vector<std::string_view> const& Number::GetParts() const {
+
+        bool String::operator==(String const& other) const {
+            return false;
         }
-        std::vector<std::string_view> const& Number::GetSuffixes() const {
+        bool String::operator!=(String const& other) const {
+            return !(*this == other);
+        }
+
+        bool Punct::operator==(Punct const& other) const {
+            return false;
+        }
+        bool Punct::operator!=(Punct const& other) const {
+            return !(*this == other);
+        }
+
+        std::string const& Token::GetSpelling() const {
+            std::visit([](auto&& arg){
+                using T = std::decay_t<decltype(arg)>;
+                if (std::is_same<T, >) {
+                } else {
+                    f
+                }
+            }, var);
+        }
+        bool HasSpaceAfter() const {
+            return flags & (1<<spaceAfterBit);
         }
     }
     std::deque<Tokeniser::Token> tokenise(std::string_view str) {
