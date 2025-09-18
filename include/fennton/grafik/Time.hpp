077e3dd66e319233e03ecf503cbf9214ee4531d7
SamFernFer <>
2025-09-18 00:54:04 -0300

    Added comments explaining the time-retrieval functions.

--- include/fennton/grafik/Time.hpp
+++ include/fennton/grafik/Time.hpp
@@ -1,7 +1,13 @@
 #include <cstdint>
 
 namespace Fennton::Grafik {
+    // Retrieves the time in seconds since Grafik's initialisation. Should not be called 
+    // if Grafik has not been initialised yet.
     double getTime();
+    // Returns the value of the raw timer. Should not be called if Grafik has not been 
+    // initialised yet.
     std::uint64_t getTimerValue();
+    // Retrieves the frequency of the raw timer. Should not be called if Grafik has not 
+    // been initialised yet.
     std::uint64_t getTimerFrequency();
 }
